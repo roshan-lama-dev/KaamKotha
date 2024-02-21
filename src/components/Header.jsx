28c16e07fad0b66edead8299a7b09d14644acbf8
roshan-dev <officialmail85@gmail.com>
2024-02-21 23:43:16 +1100

    Navbar and sign-in page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,26 @@
 import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 export const Header = () => {
   return (
-    <nav className="p-4 bg-slate-100 shadow-sm">
+    <nav className="p-4 bg-slate-700 text-white shadow-sm">
       <div className="flex items-center justify-between">
-        <span>LOGO</span>
-        <input type="text" placeholder="Search" />
+        <span className="text-3xl text-bold">LOGO</span>
+
+        <form
+          className="flex justify-center items-center gap-2"
+          action="
+        "
+        >
+          <input
+            className="outline-none text-black p-2 rounded-md"
+            type="text"
+            placeholder="Search"
+          />
+
+          <button>
+            <FaSearch className="text-2xl" />
+          </button>
+        </form>
         <ul className="flex items-center gap-3">
           <Link to="/">
             <li>Home</li>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,29 @@
-import React from "react";
-
 export const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div className=" justify-center py-44 flex-col bg-slate-200 ">
+      <div className="">
+        <p className="text-3xl text-center my-4">Sign In</p>
+        <form className="flex flex-col gap-5 justify-center items-center ">
+          <input
+            type="text"
+            className="outline-slate-500 p-3 rounded-md"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            className="outline-slate-500 p-3 rounded-md"
+            placeholder="Password"
+          />
+          <input
+            type="password"
+            className="outline-slate-500 p-3 rounded-md"
+            placeholder="Password"
+          />
+          <button className="bg-slate-700 p-3 rounded-md text-white uppercase ">
+            Sign In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
