16fad3be83938f2dad442086032a0df028f47a7b
roshan-dev <officialmail85@gmail.com>
2024-02-22 21:57:37 +1100

    Test api route controller created

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
+
+import userRouter from "./routes/user.route.js";
 const PORT = 3000;
 
 const app = express();
@@ -14,6 +16,8 @@ const databaseConnecion = () => {
   }
 };
 databaseConnecion();
+
+app.use("/api/user", userRouter);
 app.listen(PORT, () => {
   console.log(`THe server is running at http://localhost:${PORT}`);
 });
