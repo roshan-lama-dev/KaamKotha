043b091e9f4e81b977aecb34c18f9fe8d39bc4be
roshan-dev <officialmail85@gmail.com>
2024-02-23 00:07:55 +1100

    Sign In API route created

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   //   destructure the input from the user to add to the database using the user model we created
@@ -15,6 +17,28 @@ export const signup = async (req, res, next) => {
     res.status(201).json({
       message: "User created Successfully",
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(404, "Credentials not matched"));
+
+    const { password: pass, ...rest } = validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
